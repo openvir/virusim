761ab7f6fbfec88799603b984c8e85b2f8de45bc
dgrusemann <dominik.grusemann@gmail.com>
2020-01-20 22:07:23 +0100

    use periodic table

---
+++ src/utils/Formula.spec.ts
@@ -0,0 +1,7 @@
+import { getElements } from './Formula'
+
+describe('getElements', () => {
+    it('should return all elements', () => {
+        expect(getElements('OH')).toEqual({ 'O': 1, 'H': 1 })
+    })
+})
--- src/utils/Formula.ts
+++ src/utils/Formula.ts
@@ -1,16 +1,18 @@
+import { ELEMENTS } from '../models/Element'
+
 export function getElements(formula: string): Record<string, number> {
     const elements = {}
-    let rest = formula
 
-    while (rest.length > 0) {
-        const match = rest.match(/\d+/)
-        if (match) {
-            const element = rest.substring(0, match.index)
-            elements[element] = parseInt(match[0])
-            rest = rest.substring(match.index + 1)
-        } else {
-            elements[rest] = 1
-            break
+    for (let i = 0; i < formula.length; i++) {
+        const c = formula[i]
+        if (ELEMENTS[c]) {
+            const rest = formula.substring(i + 1)
+            const match = rest.match(/\d+/)
+            if (match) {
+                elements[c] = parseInt(match[0])
+            } else {
+                elements[c] = 1
+            }
         }
     }
 
