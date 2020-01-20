a65578f9248a7a0cbda46f247466ed8848d0aaf0
dgrusemann <dominik.grusemann@gmail.com>
2020-01-20 22:18:11 +0100

    add polarity

--- src/models/Bond.spec.ts
+++ src/models/Bond.spec.ts
@@ -6,5 +6,9 @@ describe('Bond', () => {
             const bond = new Bond('OH')
             expect(bond.polarity()).toEqual('polar')
         })
+        it('should consider CH a non-polar bond', () => {
+            const bond = new Bond('CH')
+            expect(bond.polarity()).toEqual('non-polar')
+        })
     })
 })
--- src/models/Bond.ts
+++ src/models/Bond.ts
@@ -1,11 +1,30 @@
+import { ELEMENTS } from './Element'
+
+import { getElements } from '../utils/Formula'
+
 export class Bond {
     name: string
+    elements: Record<string, number>
 
     constructor(name: string) {
         this.name = name
+        this.elements = getElements(name)
+        if (Object.keys(this.elements).length !== 2) {
+            throw new Error('A bond needs two elements.')
+        }
+
     }
 
     polarity(): string {
-        return 'polar'
+        let element1 = Object.keys(this.elements)[0]
+        let element2 = Object.keys(this.elements)[1]
+
+        const electroNegativity = Math.abs(ELEMENTS[element1].electronegativity_pauling - ELEMENTS[element2].electronegativity_pauling)
+
+        if (electroNegativity > 0.5) {
+            return 'polar'
+        } else {
+            return 'non-polar'
+        }
     }
 }
