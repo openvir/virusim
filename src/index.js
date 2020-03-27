2f6ea5466f73290cc1f0ac9b523c98d333b383af
dgrusemann <dominik.grusemann@gmail.com>
2020-03-27 21:28:10 +0100

    extract scene

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-let BABYLON = require('babylonjs');
+import { createScene } from './scene'
 
 var canvas = document.getElementById('renderCanvas') // Get the canvas element
 var engine = new BABYLON.Engine(canvas, true) // Generate the BABYLON 3D engine
-
-var createScene = function() {
-
-    // Create the scene space
-    var scene = new BABYLON.Scene(engine)
-
-    // Add a camera to the scene and attach it to the canvas
-    var camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene)
-    camera.attachControl(canvas, true)
-
-    // Add lights to the scene
-    var light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), scene)
-    var light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene)
-
-    // Add and manipulate meshes in the scene
-    var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene)
-
-    return scene
-}
 
 var scene = createScene() //Call the createScene function
 
