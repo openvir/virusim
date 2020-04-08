4ffe8e4ef14b85e46e16213121a3baa7574fa665
dgrusemann <dominik.grusemann@gmail.com>
2020-04-08 21:31:11 +0200

    extract method

--- src/scene.js
+++ src/scene.js
@@ -3,6 +3,21 @@ const BABYLON = require('babylonjs')
 const canvas = document.getElementById('renderCanvas') // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true) // Generate the BABYLON 3D engine
 
+function createPath(cell) {
+    const tes = 60
+    const pi2 = Math.PI * 2
+    const step = pi2 / tes
+    const path = []
+    for (let i = 0; i < pi2; i += step) {
+        const x = cell.x + cell.radius * Math.sin(i)
+        const z = 0
+        const y = cell.y + cell.radius * Math.cos(i)
+        path.push(new BABYLON.Vector3(x, y, z))
+    }
+    path.push(path[0])
+    return path
+}
+
 export const createScene = function(cells) {
     const scene = new BABYLON.Scene(engine)
     scene.clearColor = new BABYLON.Color3(.5, .5, .5)
@@ -15,17 +30,7 @@ export const createScene = function(cells) {
     light.intensity = 0.7
 
     for (const cell of cells) {
-        const tes = 60
-        const pi2 = Math.PI * 2
-        const step = pi2 / tes
-        const path = []
-        for (let i = 0; i < pi2; i += step) {
-            const x = cell.x + cell.radius * Math.sin(i)
-            const z = 0
-            const y = cell.y + cell.radius * Math.cos(i)
-            path.push(new BABYLON.Vector3(x, y, z))
-        }
-        path.push(path[0])
+        const path = createPath(cell)
         const circle = BABYLON.Mesh.CreateLines('circle', path, scene)
     }
 
