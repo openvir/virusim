e88138613c00030e673f9f6ad4917f520a882f76
dgrusemann <dominik.grusemann@gmail.com>
2020-04-10 10:27:18 +0200

    refactor - extract variable

--- src/view/Virus2d.js
+++ src/view/Virus2d.js
@@ -1,11 +1,12 @@
 const BABYLON = require('babylonjs')
 
+const PI2 = Math.PI * 2
+
 export function createVirusPath(cell) {
     const tes = 60
-    const pi2 = Math.PI * 2
-    const step = pi2 / tes
+    const step = PI2 / tes
     const path = []
-    for (let i = 0; i < pi2; i += step) {
+    for (let i = 0; i < PI2; i += step) {
         const x = cell.x + cell.radius * Math.sin(i)
         const z = 0
         const y = cell.y + cell.radius * Math.cos(i)
