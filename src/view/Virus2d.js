cea1cb232866618b74e3199c07ce1f7241090e64
dgrusemann <dominik.grusemann@gmail.com>
2020-04-10 10:35:13 +0200

    refactor - rename parameter

--- src/view/Virus2d.js
+++ src/view/Virus2d.js
@@ -2,14 +2,14 @@ const BABYLON = require('babylonjs')
 
 const PI2 = Math.PI * 2
 
-export function createVirusPath(cell) {
+export function createVirusPath(virus) {
     const tes = 60
     const step = PI2 / tes
     const path = []
     for (let i = 0; i < PI2; i += step) {
-        const x = cell.x + cell.radius * Math.sin(i)
+        const x = virus.x + virus.radius * Math.sin(i)
         const z = 0
-        const y = cell.y + cell.radius * Math.cos(i)
+        const y = virus.y + virus.radius * Math.cos(i)
         path.push(new BABYLON.Vector3(x, y, z))
     }
     path.push(path[0])
