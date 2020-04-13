a91edadd53c708386fee073ccd413eb0fafd790b
dgrusemann <dominik.grusemann@gmail.com>
2020-04-13 12:08:36 +0200

    change - render one ribosome

--- src/view/Cell2d.ts
+++ src/view/Cell2d.ts
@@ -7,6 +7,22 @@ import { LinesMesh } from '@babylonjs/core/Meshes/linesMesh'
 import { Cell } from '../models'
 
 const CELL_RADIUS = 10 // 100 μm = 100000 nm
+const RIBOSOME_RADIUS = 0.1 // 20 nm
+
+function createRibosomePoints(cell: Cell) {
+    const tes = 60
+    const pi2 = Math.PI * 2
+    const step = pi2 / tes
+    const points = []
+    for (let i = 0; i < pi2; i += step) {
+        const x = cell.x + RIBOSOME_RADIUS * Math.sin(i)
+        const z = 0
+        const y = cell.y + RIBOSOME_RADIUS * Math.cos(i)
+        points.push(new Vector3(x, y, z))
+    }
+    points.push(points[0])
+    return points
+}
 
 function createCellMembranePoints(cell: Cell) {
     const tes = 60
@@ -25,7 +41,8 @@ function createCellMembranePoints(cell: Cell) {
 
 export function createCellMesh(cell: Cell, scene: Scene): LinesMesh {
     const cellMembranePoints = createCellMembranePoints(cell)
-    const mesh = MeshBuilder.CreateLineSystem('circle', { lines: [cellMembranePoints] }, scene)
+    const ribosomePoints = createRibosomePoints(cell)
+    const mesh = MeshBuilder.CreateLineSystem('circle', { lines: [cellMembranePoints, ribosomePoints] }, scene)
 
     const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI('UI')
     const text1 = new TextBlock()
